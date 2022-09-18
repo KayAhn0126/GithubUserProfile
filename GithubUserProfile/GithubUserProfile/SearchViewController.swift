5b66abe60553b13fca6338ce17fc1d057c41dbbd
KayAhn0126 <dkseotn123@gmail.com>
2022-09-18 21:04:51 +0900

    feat: GithubUserProfile 프로젝트 추가

---
+++ GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  GithubUserSearch
+//
+//
+
+import UIKit
+import Combine
+import Kingfisher
+
+class UserProfileViewController: UIViewController {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
